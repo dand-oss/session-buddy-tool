26980278cfee0f52e89bf95ca96e2768a5067b0f
Dan Dees <dand@appsmiths.com>
2020-06-22 07:35:11 +0700

    more

--- session_buddy_tool.py
+++ session_buddy_tool.py
@@ -7,8 +7,11 @@
 # * check if exists in GetPocket and insert if not?
 # * find extension db path? what about cross-platform shit?
 
+# download the crx and unpack as zip
+# background.bundle.js
+# https://robwu.nl/crxviewer/?crx=https%3A%2F%2Fchrome.google.com%2Fwebstore%2Fdetail%2Fsession-buddy%2Fedacconmaakjimmfgnblocblbcdcpbko%3Fhl%3Den
+
 import sys
-import traceback
 import argparse
 import cjson
 import sqlite3
@@ -18,16 +21,6 @@ from pathlib import Path
 #
 # Helpers
 #
-def load_exclude_file(path):
-    excluded = []
-    if path.isfile():
-        with open(path) as f:
-            excluded = f.readlines()
-    else:
-        print("File with excluded urls can not be found!")
-    return excluded
-
-
 def extract_links(row, full):
     tabs = cjson.decode(row[1])
     row_id = None
@@ -36,31 +29,31 @@ def extract_links(row, full):
         if key == "id":
             row_id = obj
         elif key == "tabs":
-            items = []
+            item_list = []
             for i in obj:
                 if full:
-                    items.append(i)
+                    item_list.append(i)
                 else:
-                    items.append({"title": i["title"], "url": i["url"]})
-    return {"id": row_id, "items": items}
+                    item_list.append({"title": i["title"], "url": i["url"]})
+    return {"id": row_id, "item_list": item_list}
 
 
 # TODO: maybe use sets to speed things up?
-def remove_duplicates(items):
+def remove_duplicates(item_list):
     seen = []
     unique = []
-    for item in items:
+    for item in item_list:
         if not item["url"] in seen:
             seen.append(item["url"])
             unique.append(item)
     return unique
 
 
-def filter_excluded(items, excluded_urls):
+def filter_excluded(item_list, excluded_url_list):
     filtered = []
-    for item in items:
+    for item in item_list:
         found = 0
-        for url in excluded_urls:
+        for url in excluded_url_list:
             if item["url"].startswith(url):
                 found += 1
         if found == 0:
@@ -88,78 +81,64 @@ def build_tabs(id, top, width, heighttabs):
 
 def get_saved_sessions(conn, table, full):
     sessions = []
-    try:
-        cur = conn.cursor()
-        cur.execute(f"SELECT id, windows FROM {table};")
-        for row in cur.fetchall():
-            item = extract_links(row, full)
-            sessions += item["items"]
-
-    except sqlite3.Error as e:
-        print(f"Get sessions error: {e.args[0]}")
+    cur = conn.cursor()
+    cur.execute(f"SELECT id, windows FROM {table};")
+    for row in cur.fetchall():
+        item = extract_links(row, full)
+        sessions += item["item_list"]
     return sessions
 
 
-def insert_row(conn, table, row_id, items):
-    try:
-        cur = conn.cursor()
-        cur.execute(f"INSERT INTO {table} VALUES();")
-        return True
-    except sqlite3.Error as e:
-        print(f"Add merged sessions error: {e.args[0]}")
-        return False
+def insert_row(conn, table, row_id, item_list):
+    cur = conn.cursor()
+    cur.execute(f"INSERT INTO {table} VALUES();")
 
 
 def delete_row(conn, table, row_id):
-    try:
-        cur = conn.cursor()
-        cur.execute(f"DELETE * FROM {table} WHERE id={row_id};")
-        return True
-    except sqlite3.Error as e:
-        print(f"Delete error: {e.args[0]}")
-        return False
+    cur = conn.cursor()
+    cur.execute(f"DELETE * FROM {table} WHERE id={row_id};")
 
 
 #
 # Actions
 #
-def action_export(conn, tables, excluded_urls):
-    items = []
-    for table in tables:
-        items += get_saved_sessions(conn, table, False)
+def action_export(conn, table_list, excluded_url_list):
+    item_list = []
+    for table in table_list:
+        item_list += get_saved_sessions(conn, table, False)
 
-    items = remove_duplicates(filter_excluded(items), excluded_urls)
+    item_list = remove_duplicates(
+        filter_excluded(item_list),
+        excluded_url_list)
 
-    print(cjson.encode(items))
+    print(cjson.encode(item_list))
 
 
-def action_merge(conn, tables, excluded_urls):
-    items = []
-    for table in tables:
-        items += get_saved_sessions(conn, table, True)
+def action_merge(conn, table_list, excluded_url_list):
+    item_list = []
+    for table in table_list:
+        item_list += get_saved_sessions(conn, table, True)
 
-    items = remove_duplicates(filter_excluded(items), excluded_urls)
+    item_list = remove_duplicates(
+        filter_excluded(item_list),
+        excluded_url_list)
 
     # TODO: merge records
     # TODO: clear existing sessions
     # TODO: add new session with merged data
-    return None
 
 
-def action_clean(conn, tables, excluded_urls):
-    try:
-        cur = conn.cursor()
-        for table in tables:
-            cur.execute(f"DELETE * FROM {table}")
-    except sqlite3.Error as e:
-        print(f"Cleanup error: {e.args[0]}")
+def action_clean(conn, table_list, excluded_url_list):
+    cur = conn.cursor()
+    for table in table_list:
+        cur.execute(f"DELETE * FROM {table}")
 
 
 def main(argv=None):
     if not argv:
         argv = sys.argv
 
-    tables = ["SavedSessions", "PreviousSessions"]
+    table_list = ["SavedSessions", "PreviousSessions"]
 
     # handle commandline arguments
     parser = argparse.ArgumentParser()
@@ -172,48 +151,42 @@ def main(argv=None):
     parser.add_argument(
         "-e",
         "--exclude",
-        help="Path to file with excluded urls",
+        help="Path to file with excluded url_list",
         required=False)
     parser.add_argument(
         "-p",
         "--profile",
         help="Path to Chrome profile",
         required=False)
+
     args = parser.parse_args()
 
     # apply parsed commandline arguments
-    excluded_urls = []
+    excluded_url_list = []
     if args.exclude:
-        excluded_urls = load_exclude_file(args.exclude)
+        with open(args.exclude) as f:
+            excluded_url_list = f.readlines()
 
     chrome_profile = Path.home() / ".config" / "google-chrome" / "Default"
     if args.profile:
         chrome_profile = Path(args.chrome_profile)
 
-    extension = "chrome-extension_edacconmaakjimmfgnblocblbcdcpbko_0"
-    db_path = f"{chrome_profile}/databases/{extension}/2"
-    conn = sqlite3.connect(db_path)
-    try:
+    ext_id = "chrome-extension_edacconmaakjimmfgnblocblbcdcpbko_0"
+    ext_ver = 3
+    db_path = chrome_profile / ext_id / "databases" / ext_id / ext_ver
+
+    rc = 0  # assume success
+    with sqlite3.connect(db_path) as conn:
         if args.action == "export":
-            action_export(conn, tables, excluded_urls)
+            action_export(conn, table_list, excluded_url_list)
         elif args.action == "clean":
-            action_clean(conn, tables, excluded_urls)
+            action_clean(conn, table_list, excluded_url_list)
         elif args.action == "merge":
-            action_merge(conn, tables, excluded_urls)
+            action_merge(conn, table_list, excluded_url_list)
         else:
             parser.print_help()
-            sys.exit(1)
-
-        sys.exit(0)
-    except Exception as e:
-        print(f"Main error: {e}")
-        print('-'*60)
-        traceback.print_exc(file=sys.stdout)
-        print('-'*60)
-        sys.exit(1)
-    finally:
-        if conn:
-            conn.close()
+            rc = -1
+    return rc
 
 
 if __name__ == "__main__":
